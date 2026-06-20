489208756b7af37ee3ead12b3e01849ab49797dc
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-06-20 03:07:57 -0400

    Update shader.c

--- src/shader.c
+++ src/shader.c
@@ -281,11 +281,6 @@ static void emit_body(SpvBuf *out, const BodyCtx *c, uint32_t *nid)
             sb_push_n(out,w,5);
         }
 
-        STEREO_LOG(
-            "[OFFAXIS] sel(L,R)=(%f,%f)",
-            lo,
-            ro);
-
         /* left eye = +convmag, right eye = -convmag */
         {
             uint32_t w[]={op_(SpvOpSelect,6),
