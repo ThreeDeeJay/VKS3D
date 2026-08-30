e26e325db2ea6690f18ec644fd150926dc0ee049
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-08-30 18:28:37 -0400

    Revert "Reassign ViewIndex 1 to right eye"
    This reverts commit 671097e1af221ab6ed8227592ae5a53a920b3e1e.

--- src/shader.c
+++ src/shader.c
@@ -1543,8 +1543,8 @@ static bool emit_mesh_position_adjust(
                 m->ft,
                 sel,
                 isl,
-                cl,
-                cr
+                cr,
+                cl
             };
             sb_push_n(out, w, 6);
         }
