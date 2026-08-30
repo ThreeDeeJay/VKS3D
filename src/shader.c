e9710d2a1518c7ef9bc927e4e4343683d59a353a
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-08-30 18:20:30 -0400

    Reassign ViewIndex 0 to left eye

--- src/shader.c
+++ src/shader.c
@@ -1286,8 +1286,8 @@ static void emit_body(SpvBuf *out, const BodyCtx *c, uint32_t *nid)
                 m->ft,
                 sel,
                 isl,
-                c->cr,
-                c->cl
+                c->cl,
+                c->cr
             };
             sb_push_n(out, w, 6);
         }
