2648dc19e02e329651b96ea43bad995ce011c0bc
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-09-01 23:29:16 -0400

    Revert "Add present_pass as MV eligibility criterion"
    This reverts commit fdeacf816df42cacd3cd233d4494f1841cbbb103.

--- src/render_pass.c
+++ src/render_pass.c
@@ -271,13 +271,11 @@ stereo_CreateRenderPass(
     sd->stereo.enabled &&
     sd->stereo.multiview &&
     pCreateInfo->attachmentCount > 0 &&
-    !depth_only &&
-    present_pass;
+    !depth_only;
     STEREO_LOG(
-        "RenderPass classify: depth_only=%d present_pass=%d mv_eligible=%d attachments=%u fmt0=%u",
+        "RenderPass classify: depth_only=%d present_pass=%d attachments=%u fmt0=%u",
         depth_only,
         present_pass,
-        mv_eligible,
         pCreateInfo->attachmentCount,
         pCreateInfo->attachmentCount ?
         pCreateInfo->pAttachments[0].format :
