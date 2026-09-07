a4fcab32ab03f2cf45ddd5624343f4100f5e0dff
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-09-07 08:42:23 -0400

    Update shader.c

--- src/shader.c
+++ src/shader.c
@@ -956,6 +956,8 @@ static void do_scan(SpvMod *m, bool p2)
                     }
                     break;
                 }
+                break;
+            }
         } else {
             if(op==SpvOpTypePointer && wc>=4 &&
                w[i+2]==SpvStorageOutput)
