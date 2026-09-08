d5dc05ec7a282a4a293d58a1cf677a0d2dc70100
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-09-08 06:20:07 -0400

    Don't flatten GS as background

--- src/shader.c
+++ src/shader.c
@@ -10966,7 +10966,7 @@ stereo_CreateGraphicsPipelines(VkDevice device, VkPipelineCache pc,
                 &patched, &pc2,
                 lo, ro, conv,
                 true,
-                vs_background,
+                false,
                 dbgG))
             {
                 STEREO_LOG(
