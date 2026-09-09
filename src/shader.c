7fbc09b2b1b14a177c5ae26f4d3890f0d47cb759
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-09-09 05:51:04 -0400

    Revert "Temporarily remove edge expansion write block"
    This reverts commit ed755c5fe99aac464ceb641d349def81a9b09c2e.

--- src/shader.c
+++ src/shader.c
@@ -1468,6 +1468,16 @@ static void emit_body(SpvBuf *out, const BodyCtx *c, uint32_t *nid)
             };
             sb_push_n(out, w3, 5);
         }
+        {
+            uint32_t w4[] = {
+                op_(SpvOpFSub, 5),
+                m->ft,
+                nx2,
+                bg_x,
+                bg_offset
+            };
+            sb_push_n(out, w4, 5);
+        }
         STEREO_LOG(
             "VS_BACKGROUND "
             "x=%u "
