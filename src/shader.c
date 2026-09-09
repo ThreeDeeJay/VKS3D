f0d541d279fb47ec50e2d5f450e5bf7caf11e506
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-09-09 05:56:23 -0400

    Update shader.c

--- src/shader.c
+++ src/shader.c
@@ -1463,8 +1463,8 @@ static void emit_body(SpvBuf *out, const BodyCtx *c, uint32_t *nid)
                 op_(SpvOpFMul, 5),
                 m->ft,
                 bg_x,
-                px,
-                bg_scale
+                bg_offset,
+                pw
             };
             sb_push_n(out, w3, 5);
         }
