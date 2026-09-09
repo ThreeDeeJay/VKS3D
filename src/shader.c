475163a087e8d63ea175cffd6a98db5ab265fcd4
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-09-09 10:04:58 -0400

    Revert "Gate mono UI on quad pipeline"
    This reverts commit 459a073fced4e0a1cd9bcb9ce60b9c7c459f8f3b.

--- src/shader.c
+++ src/shader.c
@@ -2606,7 +2606,6 @@ bool spirv_patch_stereo_vertex(
     if (cfg && cfg->mono_ui) {
         bool ui_candidate =
         dbg &&
-        is_quad &&
         m.pos_var != 0 &&
         !m.has_matrix_ops &&
         m.has_position_input &&
