88a5ee6587aee4a9b00263149600d31e3eec3847
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-09-09 06:05:49 -0400

    Update shader.c

--- src/shader.c
+++ src/shader.c
@@ -1469,14 +1469,14 @@ static void emit_body(SpvBuf *out, const BodyCtx *c, uint32_t *nid)
             sb_push_n(out, w3, 5);
         }
         {
-            uint32_t w4[] = {
+            uint32_t w3[] = {
                 op_(SpvOpFSub, 5),
                 m->ft,
                 nx2,
-                bg_x,
-                bg_offset
+                px,
+                bg_x
             };
-            sb_push_n(out, w4, 5);
+            sb_push_n(out, w3, 5);
         }
         STEREO_LOG(
             "VS_BACKGROUND "
