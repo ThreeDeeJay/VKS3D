4fb9083673df237d3836083fd0e40a8366a0ba15
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-09-11 03:43:52 -0400

    Reapply "Reapply "Restore background edge expansion""
    This reverts commit 21fec288124557ff340abe78f02b7aa5aa76b1b2.

--- src/shader.c
+++ src/shader.c
@@ -1478,6 +1478,7 @@ static void emit_body(SpvBuf *out, const BodyCtx *c, uint32_t *nid)
     if (c->force_far_depth)
     {
         uint32_t bg_offset = (*nid)++;
+        uint32_t bg_scale = (*nid)++;
         uint32_t bg_x = (*nid)++;
         uint32_t w[] = {
             op_(SpvOpFMul, 5),
@@ -1489,23 +1490,33 @@ static void emit_body(SpvBuf *out, const BodyCtx *c, uint32_t *nid)
         sb_push_n(out, w, 5);
         {
             uint32_t w2[] = {
-                op_(SpvOpFMul, 5),
+                op_(SpvOpFAdd, 5),
                 m->ft,
-                bg_x,
-                bg_offset,
-                pw
+                bg_scale,
+                c->bg_expand,
+                c->cf1
             };
             sb_push_n(out, w2, 5);
         }
         {
             uint32_t w3[] = {
+                op_(SpvOpFMul, 5),
+                m->ft,
+                bg_x,
+                px,
+                bg_scale
+            };
+            sb_push_n(out, w3, 5);
+        }
+        {
+            uint32_t w4[] = {
                 op_(SpvOpFSub, 5),
                 m->ft,
                 nx2,
-                px,
-                bg_x
+                bg_x,
+                bg_offset
             };
-            sb_push_n(out, w3, 5);
+            sb_push_n(out, w4, 5);
         }
         STEREO_LOG(
             "VS_BACKGROUND "
@@ -1513,11 +1524,15 @@ static void emit_body(SpvBuf *out, const BodyCtx *c, uint32_t *nid)
             "x2=%u "
             "convergence=%u "
             "stereo_offset=%u "
+            "expand=%u "
+            "scale=%u "
             "w=%u",
             px,
             nx2,
             c->cc,
             bg_offset,
+            c->bg_expand,
+            bg_scale,
             pw);
     }
     else
