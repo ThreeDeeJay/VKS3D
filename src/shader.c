17f61cc6c8a3f0015cc1240b35358d4c92e27cc8
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-09-11 09:03:39 -0400

    Extend flattened skybox boundaries

--- src/shader.c
+++ src/shader.c
@@ -1540,25 +1540,36 @@ static void emit_body(SpvBuf *out, const BodyCtx *c, uint32_t *nid)
         }
         else
         {
+            uint32_t bg_scale = (*nid)++;
             {
                 uint32_t w2[] = {
-                    op_(SpvOpFMul, 5),
+                    op_(SpvOpFAdd, 5),
                     m->ft,
-                    bg_x,
-                    bg_offset,
-                    pw
+                    bg_scale,
+                    c->bg_expand,
+                    c->cf1
                 };
                 sb_push_n(out, w2, 5);
             }
             {
                 uint32_t w3[] = {
+                    op_(SpvOpFMul, 5),
+                    m->ft,
+                    bg_x,
+                    px,
+                    bg_scale
+                };
+                sb_push_n(out, w3, 5);
+            }
+            {
+                uint32_t w4[] = {
                     op_(SpvOpFSub, 5),
                     m->ft,
                     nx2,
-                    px,
-                    bg_x
+                    bg_x,
+                    bg_offset
                 };
-                sb_push_n(out, w3, 5);
+                sb_push_n(out, w4, 5);
             }
             STEREO_LOG(
                 "VS_BACKGROUND_FLAT "
@@ -1566,11 +1577,15 @@ static void emit_body(SpvBuf *out, const BodyCtx *c, uint32_t *nid)
                 "x2=%u "
                 "convergence=%u "
                 "stereo_offset=%u "
+                "expand=%u "
+                "scale=%u "
                 "w=%u",
                 px,
                 nx2,
                 c->cc,
                 bg_offset,
+                c->bg_expand,
+                bg_scale,
                 pw);
         }
     }
