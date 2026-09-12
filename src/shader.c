98c0651b6a56b81c4e2e3d5525e52bf1212c79ce
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-09-12 03:43:49 -0400

    Fix sky background ID generation

--- src/shader.c
+++ src/shader.c
@@ -1563,16 +1563,6 @@ static void emit_body(SpvBuf *out, const BodyCtx *c, uint32_t *nid)
                     };
                     sb_push_n(out, w3, 5);
                 }
-                {
-                    uint32_t w4[] = {
-                        op_(SpvOpFSub, 5),
-                        m->ft,
-                        nx2,
-                        bg_x,
-                        bg_offset
-                    };
-                    sb_push_n(out, w4, 5);
-                }
                 STEREO_LOG(
                     "VS_BACKGROUND_FLAT "
                     "x=%u "
@@ -1591,6 +1581,16 @@ static void emit_body(SpvBuf *out, const BodyCtx *c, uint32_t *nid)
                     pw);
             }
             else
+            {
+                uint32_t w3[] = {
+                    op_(SpvOpFMul, 5),
+                    m->ft,
+                    bg_x,
+                    px,
+                    c->cf1
+                };
+                sb_push_n(out, w3, 5);
+            }
             {
                 uint32_t w4[] = {
                     op_(SpvOpFSub, 5),
