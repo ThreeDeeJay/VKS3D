236e79126fcff251e354928f7c497544daa4bb41
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-09-12 02:54:12 -0400

    Limit sky expansion to direct-position shaders

--- src/shader.c
+++ src/shader.c
@@ -1540,27 +1540,57 @@ static void emit_body(SpvBuf *out, const BodyCtx *c, uint32_t *nid)
         }
         else
         {
-            uint32_t bg_scale = (*nid)++;
+            if (m->has_direct_position_write && !m->has_matrix_ops)
             {
-                uint32_t w2[] = {
-                    op_(SpvOpFAdd, 5),
-                    m->ft,
-                    bg_scale,
-                    c->bg_expand,
-                    c->cf1
-                };
-                sb_push_n(out, w2, 5);
-            }
-            {
-                uint32_t w3[] = {
-                    op_(SpvOpFMul, 5),
-                    m->ft,
-                    bg_x,
+                uint32_t bg_scale = (*nid)++;
+                {
+                    uint32_t w2[] = {
+                        op_(SpvOpFAdd, 5),
+                        m->ft,
+                        bg_scale,
+                        c->bg_expand,
+                        c->cf1
+                    };
+                    sb_push_n(out, w2, 5);
+                }
+                {
+                    uint32_t w3[] = {
+                        op_(SpvOpFMul, 5),
+                        m->ft,
+                        bg_x,
+                        px,
+                        bg_scale
+                    };
+                    sb_push_n(out, w3, 5);
+                }
+                {
+                    uint32_t w4[] = {
+                        op_(SpvOpFSub, 5),
+                        m->ft,
+                        nx2,
+                        bg_x,
+                        bg_offset
+                    };
+                    sb_push_n(out, w4, 5);
+                }
+                STEREO_LOG(
+                    "VS_BACKGROUND_FLAT "
+                    "x=%u "
+                    "x2=%u "
+                    "convergence=%u "
+                    "stereo_offset=%u "
+                    "expand=%u "
+                    "scale=%u "
+                    "w=%u",
                     px,
-                    bg_scale
-                };
-                sb_push_n(out, w3, 5);
+                    nx2,
+                    c->cc,
+                    bg_offset,
+                    c->bg_expand,
+                    bg_scale,
+                    pw);
             }
+            else
             {
                 uint32_t w4[] = {
                     op_(SpvOpFSub, 5),
@@ -1571,22 +1601,6 @@ static void emit_body(SpvBuf *out, const BodyCtx *c, uint32_t *nid)
                 };
                 sb_push_n(out, w4, 5);
             }
-            STEREO_LOG(
-                "VS_BACKGROUND_FLAT "
-                "x=%u "
-                "x2=%u "
-                "convergence=%u "
-                "stereo_offset=%u "
-                "expand=%u "
-                "scale=%u "
-                "w=%u",
-                px,
-                nx2,
-                c->cc,
-                bg_offset,
-                c->bg_expand,
-                bg_scale,
-                pw);
         }
     }
     else
