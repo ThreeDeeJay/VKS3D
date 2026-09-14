785534fa51d38465d6c38d1b91088dc00e4644fe
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-09-14 06:22:45 -0400

    Update shader.c

--- src/shader.c
+++ src/shader.c
@@ -11942,7 +11942,7 @@ stereo_CreateGraphicsPipelines(VkDevice device, VkPipelineCache pc,
     }
     STEREO_LOG(
         "DUAL_PATCH_STATE p=%u vs=%p fs=%p stages=%u",
-        p,
+        dbg_p,
         (void*)tmp_vs_mod[p],
         (void*)tmp_fs_mod[p],
         infos[p].stageCount);
